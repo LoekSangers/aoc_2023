c92a4202db828dca4ca9806e2d421d908c0ba52c
Loek Sangers <9132841+LoekSangers@users.noreply.github.com>
2023-12-07 16:09:18 +0000

    finally got day 5 done :)

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -44,7 +44,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let mut groups = input.split("\n\n");
 
-    let parsing = groups.next().unwrap()
+    let seeds = groups.next().unwrap()
         .split_ascii_whitespace()
         .filter_map(|seed| {
             if seed.as_bytes()[0].is_ascii_digit(){
@@ -52,26 +52,16 @@ pub fn part_two(input: &str) -> Option<u32> {
             }else{
                 None
             }
-        }).collect::<Vec<i64>>();
-    let seeds = parsing
-        .chunks(2)
-        .flat_map(|slice| {
-            (slice[0]..slice[0]+slice[1]).collect::<Vec<i64>>()
         });
 
-    println!("{:#?}", seeds.clone().collect::<Vec<i64>>().len());
 
     let steps = groups.map(|step|{
-        let mut parts = step.lines().skip(1).map(|line|{
-            let args: Vec<u64> = line.split_ascii_whitespace().map(|n| n.parse::<u64>().ok().unwrap()).collect();
-        
-            (args[0] as i64, (args[0] + args[2]) as i64, args[0] as i64 - args[1] as i64)
-        }).collect::<Vec<(i64, i64, i64)>>();
-
-        parts.sort();
-
-        parts
-    }).collect::<Vec<Vec<(i64, i64, i64)>>>();
+            step.lines().skip(1).map(|line|{
+                let args: Vec<u64> = line.split_ascii_whitespace().map(|n| n.parse::<u64>().ok().unwrap()).collect();
+            
+                (args[1] as i64, (args[1] + args[2]) as i64, args[1] as i64 - args[0] as i64)
+            })
+        });
 
     // Try with ranges as seed instead of numbers
     let result = seeds
@@ -81,24 +71,78 @@ pub fn part_two(input: &str) -> Option<u32> {
 
         let mut ranges = vec![(seed[0], seed[0] + seed[1])];
         for step in steps.clone().into_iter(){
-            let mut new_ranges : Vec<(i64, i64)> = Vec::new();
+            let mut mapped_ranges : Vec<(i64, i64)> = Vec::new();
             
             //transform the range into the new range(s) note that one range can become multiple
-            for range in ranges {
-                let diff = match step.clone().find(|(start, end, _)| {
-                    start <= &tmp_val && &tmp_val <= end
-                }) {
-                    Some(x) => x.2,
-                    None => 0
-                };
-                tmp_val -= diff;
+            for mapping in step {
+                let mut unmapped_ranges = Vec::new();
 
+                for range in ranges {
+                    // m0---m1
+                    //          r0---r1
+                    if mapping.1 < range.0 {
+                        unmapped_ranges.push(range);
+                    }
+                    
+                    //          m0---m1
+                    // r0---r1
+                    else if range.1 < mapping.0 {
+                        unmapped_ranges.push(range);
+                    }
+                    
+                    
+                    //     m0-----m1
+                    // r0-----r1
+                    else if range.0 <= mapping.0 && range.1 >= mapping.0 && range.1 <= mapping.1 {
+                        if range.0 < mapping.0 {
+                            unmapped_ranges.push((range.0, mapping.0-1));
+                        }
+                        mapped_ranges.push((mapping.0 - mapping.2, range.1 - mapping.2));
+                    }
+
+                    // m0-----m1
+                    //    r0-----r1
+                    else if mapping.0 <= range.0 && mapping.1 >= range.0 && mapping.1 <= range.1 {
+                        if mapping.1 < range.1 {
+                            unmapped_ranges.push((mapping.1+1, range.1));
+                        }
+                        mapped_ranges.push((range.0 - mapping.2, mapping.1 - mapping.2));
+                    }
+
+                    
+                    
+                    // m0---------m1
+                    //    r0---r1
+                    else if mapping.0 <= range.0 && mapping.1 >= range.1{
+                        mapped_ranges.push((range.0 - mapping.2, range.1 - mapping.2));
+                    }
+                    
+                    
+                    //    m0--m1
+                    // r0---------r1
+                    else if range.0 <= mapping.0 && range.1 >= mapping.1{
+                        mapped_ranges.push((mapping.0 - mapping.2, mapping.1 - mapping.2));
+                        if range.0 < mapping.0{
+                            unmapped_ranges.push((range.0, mapping.0-1));
+
+                        }
+                        if mapping.1 < range.1{
+                            unmapped_ranges.push((mapping.1, range.1));
+
+                        }
+                    }
+
+                    else {  
+                        panic!("missed mapping");
+                    }
+
+                }
+                ranges = unmapped_ranges
             }
-
-            ranges = new_ranges;
+            ranges.extend(mapped_ranges);
         }
         ranges
-    }).map(|range|  range.iter().min().unwrap().0 as u32 ).collect::<Vec<u32>>();
+    }).map(|ranges|  ranges.iter().min().unwrap().0 as u32 ).collect::<Vec<u32>>();
     
     Some(*result.iter().min().unwrap())
 }
--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,15 +1,30 @@
+use std::iter::zip;
+
 advent_of_code::solution!(6);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let lines = input.lines().collect::<Vec<&str>>();
+
+    let time = lines[0].split_ascii_whitespace().skip(1).map(|s| s.parse::<f64>().ok().unwrap());
+    let dist = lines[1].split_ascii_whitespace().skip(1).map(|s| s.parse::<f64>().ok().unwrap());
+
+    Some(zip(time, dist).map(|(t, d)| {
+        1 + (((t+(t*t-4_f64*(d+1_f64)).powf(0.5_f64))/2_f64 as f64 ).floor() - ((t-(t*t-4_f64*(d+1_f64)).powf(0.5_f64))/2_f64 as f64).ceil()) as u32
+    }).reduce(|a, b| a * b).unwrap())
 
 // with Wolfram alpha:
 // 1 + floor((t+sqrt(t^2-4*(d+1)))/2) - ceil((t-sqrt(t^2-4*(d+1)))/2) for t = 30 and d = 200
 
-    None
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let lines = input.lines().collect::<Vec<&str>>();
+
+    let t = lines[0].split(':').skip(1).next().unwrap().chars().filter(|c| !c.is_whitespace()).collect::<String>().parse::<f64>().ok().unwrap();
+    let d = lines[1].split(':').skip(1).next().unwrap().chars().filter(|c| !c.is_whitespace()).collect::<String>().parse::<f64>().ok().unwrap();
+    Some(
+        1 + (((t+(t*t-4_f64*(d+1_f64)).powf(0.5_f64))/2_f64 as f64 ).floor() - ((t-(t*t-4_f64*(d+1_f64)).powf(0.5_f64))/2_f64 as f64).ceil()) as u32
+    )
 }
 
 #[cfg(test)]
@@ -19,12 +34,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(288));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71503));
     }
 }
